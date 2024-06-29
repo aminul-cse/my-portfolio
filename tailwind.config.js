c97965b3b7ea84a33e83f1c6ceed6b39bff215fd
Aminul Islam <aminulcse2023@gmail.com>
2024-06-29 07:45:15 +0600

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
+    fontFamily: {
+      "TimesNewRoman": ["TimesNewRoman", "Times New Roman", "Times", "Baskerville", "Georgia", "serif"],
+      "clicker": ["Clicker Script", "cursive"]
+    },
     extend: {
       colors: {
-        'green': '#37b182',
+        green: "#37b182",
       },
     },
   },
   plugins: [],
-}
+};
